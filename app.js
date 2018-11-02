b52498fe9c571a7075593aa3f2d70c4beb5e528c
Scott Hurlow <Scottyhurlow@gmail.com>
2018-11-02 09:00:21 -0700

    cleanup master branch

--- app.js
+++ app.js
@@ -39,68 +39,7 @@ const authors = [
   Bonus challenge: Make middleware that checks if the requested author index exists, respond 404 if not.
 */
 
-app.get('/authors', (req, res) => {
-  res.send(authors)
-})
 
-app.post('/authors', (req, res) => {
-  const newAuthor = req.body.name
-  authors.push(newAuthor)
-  res.status(201).send(newAuthor)
-})
-
-app.get('/authors/:id', (req, res, next) => {
-  const authorIndex = req.params.id
-  const author = authors[authorIndex]
-  
-  if(!author) {
-    next()
-  }
-
-  res.send(author)
-})
-
-app.get('/authors/:id/books', (req, res) => {
-  const authorIndex = req.params.id
-  const author = authors[authorIndex]
-  
-  if (!author) {
-    next()
-  }
-
-  res.send(authors[authorIndex].books)
-})
-
-app.post('/authors/:id/books', (req, res) => {
-  const authorIndex = req.params.id
-  const author = authors[authorIndex]
-  
-  if (!author) {
-    next()
-  }
-
-  const newBook = req.body.title
-  authors[authorIndex].books.push(newBook)
-  res.status(201).send(newBook)
-})
-
-app.delete('/authors/:id/books/:bookId', (req, res) => {
-  const authorIndex = req.params.id
-  const bookIndex = req.params.bookId
-  const author = authors[authorIndex]
-  
-  if (!author) {
-    next()
-  }
-
-  if(!author.books[bookIndex]) {
-    nex()
-  }
-
-  author.books.splice(bookIndex, 1)
-  res.send(author.books)
-
-})
 
 app.use((req, res) => {
   res.sendStatus(404)
