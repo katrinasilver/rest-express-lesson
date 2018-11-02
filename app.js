1b7f5ee3ad54c0b1686042e6678c25bdb7f58636
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-02 11:38:12 -0700

    wip

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const authors = [
 
   - GET author by index
     - should respond 404 if author at index does not exist
-  - POST author  
+  - POST author
   - GET all books by author
   - POST book by author
     - should respond 404 if author does not exist
@@ -35,14 +35,28 @@ const authors = [
     - should respond 404 if author does not exist
   - DELETE specific book by author
     - should respond 404 if author does not exist
-  
+
   Bonus challenge: Make middleware that checks if the requested author index exists, respond 404 if not.
 */
+app.use(express.json())
+app.route('/authors')
+  .get((req, res, next) => {
+    if (!authors) next() // send a 404
+    res.send(authors)
+  })
+  // .post((req, res) => {
+  //   let add = authors.push(req.body.name)
+  //   console.log(add)
+  //   res.send({add})
+  // })
 
-
+app.get('/authors/:id', (req, res) => {
+  let query = req.params.id
+  res.send(authors[query])
+})
 
 app.use((req, res) => {
-  res.sendStatus(404)
+  res.sendStatus(404, `Not found ${err.stack}`)
 })
 
 
